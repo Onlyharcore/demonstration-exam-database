bfaa81ad80c24710a061b03bd85fd31a2aae5f03
Onlyharcore <dia021198@mail.ru>
2026-09-05 17:58:16 +0300

    Русские алиасы в аналитических запросах

--- 10_analytics_5.sql
+++ 10_analytics_5.sql
@@ -1,14 +1,14 @@
 -- Аналитика 5: статистика по экзаменам.
 
 SELECT
-    e.name AS exam_name,
-    e.exam_level,
-    COUNT(er.result_id) AS participants_count,
-    ROUND(AVG(er.score*100.0/er.max_score),2) AS average_score_percent,
-    ROUND(COUNT(*) FILTER (WHERE er.passed=TRUE)*100.0/COUNT(*),2) AS pass_rate_percent
+    e.name AS "Экзамен",
+    e.exam_level AS "Уровень экзамена",
+    COUNT(er.result_id) AS "Количество участников",
+    ROUND(AVG(er.score * 100.0 / er.max_score), 2) AS "Средний результат, %",
+    ROUND(COUNT(*) FILTER (WHERE er.passed = TRUE) * 100.0 / COUNT(*), 2) AS "Доля сдавших, %"
 FROM exams e
-JOIN exam_sessions es ON es.exam_id=e.exam_id
-JOIN registrations r ON r.session_id=es.session_id
-JOIN exam_results er ON er.registration_id=r.registration_id
-GROUP BY e.exam_id,e.name,e.exam_level
-ORDER BY average_score_percent DESC;
+JOIN exam_sessions es ON es.exam_id = e.exam_id
+JOIN registrations r ON r.session_id = es.session_id
+JOIN exam_results er ON er.registration_id = r.registration_id
+GROUP BY e.exam_id, e.name, e.exam_level
+ORDER BY "Средний результат, %" DESC;
