9edafe51a58d444697258d1710819758f55b9433
Onlyharcore <dia021198@mail.ru>
2026-09-05 17:58:10 +0300

    Русские алиасы в аналитических запросах

--- 09_analytics_4.sql
+++ 09_analytics_4.sql
@@ -1,10 +1,10 @@
 -- Аналитика 4: средний процент выполнения заданий.
 
 SELECT
-    task_number,
-    task_type,
-    COUNT(*) AS results_count,
-    ROUND(AVG(score*100.0/max_score),2) AS average_task_percent
+    task_number AS "Номер задания",
+    task_type AS "Тип задания",
+    COUNT(*) AS "Количество результатов",
+    ROUND(AVG(score * 100.0 / max_score), 2) AS "Средний процент выполнения, %"
 FROM task_results
-GROUP BY task_number,task_type
-ORDER BY task_number;
+GROUP BY task_number, task_type
+ORDER BY "Номер задания";
